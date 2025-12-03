07975bcafaa9df084c2305e2aec0fcf5b17dc24c
Fran1497 <franciscocaravacacastillo7@gmail.com>
2025-12-03 13:31:23 +0100

    camion helados

---
+++ src/Ejercicios_debug/objeto/Practicacloneydemas/MainCamion.java
@@ -0,0 +1,35 @@
+package Ejercicios_debug.objeto.Practicacloneydemas;
+
+import java.util.Scanner;
+
+public class MainCamion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CamionHelados model1 = new CamionHelados("model1", 4000);
+        CamionHelados model2 = new CamionHelados("model2", 4000);
+        CamionHelados model3 = (CamionHelados) model1.clone();
+
+
+        System.out.println(model1.toString());
+        System.out.println(model2.toString());
+        System.out.println("\n");
+
+        System.out.println(model1.equals(model2));
+        System.out.println("\n");
+
+        System.out.println(model1.modelo + " - " + model1.kilometros);
+        System.out.println(model3.modelo + " - " + model3.kilometros +"\n");
+
+        System.out.println(model1.getinfo());
+
+
+        System.out.println("Cuantas personas atiendes: " + "\n");
+
+        int personas = scanner.nextInt();
+        model1.atenderpersonas(personas);
+        System.out.println(model1.getinfo());
+
+    }
+
+}
+
