ef478188bffa75b53b4a536da1dfcff65e6c8c8b
Francisco <franciscocaravacacastillo7@gmail.com>
2026-02-18 22:02:25 +0100

    app Cartera.java

--- src/Ejercicios_debug/clases/activopkg/Cartera.java
+++ src/Ejercicios_debug/clases/activopkg/Cartera.java
@@ -6,18 +6,15 @@ import java.util.List;
 public class Cartera{
     private  List<Operacion> operaciones = new ArrayList<>();
 
-
-
-
-
     public void comprar(Activo activo, double cantidad, double precio){
         operaciones.add(new Operacion("C",cantidad,precio,activo));
     }
     public void vender(Activo activo, double cantidad, double precio){
         operaciones.add(new Operacion("V",cantidad,precio,activo));
     }
-    public void  traspasar(FondoInversion FondoInversion1, double precioorig,double cantidadorig, FondoInversion FondoInversion2, double preciofin,double cantidadfin ){
-        operaciones.add(new Operacion("T",preciofin,cantidadorig,FondoInversion2,preciofin,cantidadfin));
+    public void  traspasar(FondoInversion FondoInversion1, double precioorig,double cantidadorig, FondoInversion FondoInversion2, double preciofin,double cantidadfin, Activo activo ){
+        operaciones.add(new Operacion("T",precioorig,cantidadfin,activo));
+        operaciones.add(new Operacion("T",preciofin,cantidadfin,activo));
     }
 
 
