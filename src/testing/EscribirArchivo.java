29ea87ccf8f9e11dd613faa9f489b9181e4d28df
Francisco <franciscocaravacacastillo7@gmail.com>
2026-03-15 12:57:20 +0100

    EscribirArchivo.java

---
+++ src/testing/EscribirArchivo.java
@@ -0,0 +1,15 @@
+package testing;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class EscribirArchivo {
+    public static void main(String[] args) {
+        try (FileWriter writer = new FileWriter("src/Testing/salidas/resultado.txt")) {
+            writer.write("Hola Francisco, este es el resultado.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
