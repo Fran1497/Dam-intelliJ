29c3de70e408e5a1542099331516ab472f21dfc2
Fran1497 <franciscocaravacacastillo7@gmail.com>
2026-04-20 14:46:54 +0200

    SWING Java

--- src/ejercicios_debug/swing/_3_Layout.java
+++ src/ejercicios_debug/swing/_3_Layout.java
@@ -21,27 +21,24 @@ public class _3_Layout extends JFrame {
         panelCentral.add(new JLabel("Usuario: "));
         textfieldNombre = new JTextField();
         textfieldNombre.setColumns(10);
-        textfieldNombre.addActionListener(e -> calcular());
+        textfieldNombre.addActionListener(e -> calcular1());
         panelCentral.add(textfieldNombre);
         panelCentral.add(new JLabel("Contraseña "));
         textfieldPass = new JTextField(10);
         panelCentral.add(textfieldPass);
-        textfieldPass.addActionListener(e -> calcular());
+        textfieldPass.addActionListener(e -> calcular1());
         panelPrincipal.add(panelCentral, BorderLayout.CENTER);
         panelPrincipal.add(botonAceptar, BorderLayout.SOUTH);
         this.pack();
         this.setVisible(true);
     }
 
-    public void calcular(){
-        JOptionPane.showMessageDialog(
-                this, (
-                        textfieldNombre.getText().equals(textfieldPass.getText().equals("1234"))
-                        ?"Correcto"
-                                :"Incorrecto"
-                        )
-        );
-
+    public void calcular1 (){
+        if (textfieldPass.getText().equals("1234") && (textfieldNombre.getText().equals("Paco")))  {
+            JOptionPane.showMessageDialog(this , "Contraseña correcta " + textfieldNombre.getText());
+        } else {
+            JOptionPane.showMessageDialog(this, "Contraseña o Usuario incorrectos");
+        }
     }
     public static void main(String[] args) {
         new _3_Layout();
