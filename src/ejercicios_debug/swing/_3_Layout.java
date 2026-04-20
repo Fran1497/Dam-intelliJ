8105077b548fde252c71a2e0e8fe681cc8f9bcda
Francisco <franciscocaravacacastillo7@gmail.com>
2026-04-20 23:04:07 +0200

    swing calc

--- src/ejercicios_debug/swing/_3_Layout.java
+++ src/ejercicios_debug/swing/_3_Layout.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class _3_Layout extends JFrame {
     JPanel panelPrincipal;
     JPanel panelCentral;
+
     JTextField textfieldNombre;
     JTextField textfieldPass;
     JButton botonAceptar;
@@ -14,34 +15,34 @@ public class _3_Layout extends JFrame {
         super("Nueva ventana");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+
         panelPrincipal = new JPanel();
-        panelCentral = new JPanel();
         panelPrincipal.setLayout(new BorderLayout());
-        panelCentral.setLayout(new GridLayout(4,4));
-        panelCentral.add(new JLabel("Usuario: "));
-        textfieldNombre = new JTextField();
-        textfieldNombre.setColumns(10);
-        textfieldNombre.addActionListener(e -> calcular1());
-        panelCentral.add(textfieldNombre);
-        panelCentral.add(new JLabel("Contraseña "));
-        textfieldPass = new JTextField(10);
-        panelCentral.add(textfieldPass);
-        textfieldPass.addActionListener(e -> calcular1());
+
+        // arriba 1 button
+        JButton botonArriba = new JButton("Arriba");
+        panelPrincipal.add(botonArriba, BorderLayout.NORTH);
+
+        // Centro 4 buttons
+        panelCentral = new JPanel();
+        panelCentral.setLayout(new GridLayout(2,2));
+        panelCentral.add(new JButton("C1"));
+        panelCentral.add(new JButton("C2"));
+        panelCentral.add(new JButton("C3"));
+        panelCentral.add(new JButton("C4"));
         panelPrincipal.add(panelCentral, BorderLayout.CENTER);
+
+        // abajo 1 button
+        botonAceptar = new JButton("Abajo");
         panelPrincipal.add(botonAceptar, BorderLayout.SOUTH);
+
+        this.add(panelPrincipal);
         this.pack();
         this.setVisible(true);
     }
 
-    public void calcular1 (){
-        if (textfieldPass.getText().equals("1234") && (textfieldNombre.getText().equals("Paco")))  {
-            JOptionPane.showMessageDialog(this , "Contraseña correcta " + textfieldNombre.getText());
-        } else {
-            JOptionPane.showMessageDialog(this, "Contraseña o Usuario incorrectos");
-        }
-    }
     public static void main(String[] args) {
         new _3_Layout();
     }
-
 }
+
